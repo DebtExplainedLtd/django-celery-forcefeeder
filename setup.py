e0d7ea84e8d6980d09566b7fb7528d831ff99c3a
Hayden Crocker <hayden@debtxplained.com>
2017-04-11 20:54:34 +0100

    Added setup.py to be able to install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Celery ForceFeeder',
+      version='1.0.0',
+      description='Execute Celery tasks from the Django admin view',
+      author='Hayden Crocker',
+      author_email='hayden.crocker@debtexplained.com',
+      packages=['feeder'],
+      )
